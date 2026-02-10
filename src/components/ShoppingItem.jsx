68a93ed31e6ea528cb57934473c12536b7e58221
StormyDoom <jesper.simensen19@gmail.com>
2026-02-10 17:23:17 +0100

    Kommentar ang AI bruk

--- src/components/ShoppingItem.jsx
+++ src/components/ShoppingItem.jsx
@@ -6,6 +6,8 @@ export default function ShoppingItem({checked, id, title, number, setShoppingLis
         setShoppingList(prev => prev.map(item => item.id === id ? {...item, number: value}: item))
     }
 
+    //Linje 11 til 14 er en funksjon jeg har fått fra CoPilot. Lenke til samtalen: https://copilot.microsoft.com/shares/mQMC1viPcW1eLxMjWgduG OBS: Dette er en chat som jeg har prøvd å kopiere fra Windows panel til chat på CoPilot nettsiden. Det går ikke ann å kopiere lenke fra CoPilot Windows panel
+
     function handleCheckboxChange(e){
         const value = e.target.checked;
         setCheckedItems(prev => prev.map(item => item.id === id ? {...item, checked: value}: item))
